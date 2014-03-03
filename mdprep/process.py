236da6bdb445699a768f6dd2d5ba4e1fb147627d
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-03-03 11:51:44 -0500

    issue #1: add OptCommand

--- mdprep/process.py
+++ mdprep/process.py
@@ -1,5 +1,6 @@
 from . import log
 
+import os
 import subprocess
 import shlex
 
@@ -18,9 +19,50 @@ class Process(object):
         if log is not None:
             with open(log, mode) as fd:
                 fd.write(err + '\n' + out)
+        return out, err
+
+class OptCommand(object):
+    """
+    A process that can take short-form command line parameters as keyword
+    arguments.  The order is arguments is not stable, this can only be
+    used when the order is unimportant.
+
+    e.g.:
+    Given: foo -a arga -b argb -c
+    Write: OptCommand('foo')(a='arga', b='argb', c=True)
+
+    Returns: (stdout, stderr)
+    """
+
+    def __init__(self, cmd):
+        self._cmd = cmd
+
+    def __call__(self, **kws):
+        # prepare the parameters
+        parms = list()
+        for k, v in kws.iteritems():
+            flag = '-' + k
+            if v is None: continue
+            elif type(v) is bool and v:
+                arg = flag
+            else:
+                arg = '%s %s' % (flag, v)
+            parms.append(arg)
+        parms = ' '.join(parms)
+
+        # the command
+        cmd = self._cmd + ' ' + parms
+
+        # run
+        return Process(cmd).run()
 
 def proc(name, *supers):
     supers = supers or (Process,)
     cls    = type(name, supers, {})
     cls.__name__ = name
     return cls
+
+def optcmd(cmd, name=None):
+    name = name if name is not None else os.path.basename(cmd)
+    cls = type(name, (OptCommand,), {})
+    return cls(cmd)
