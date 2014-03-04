1bec127fa61e911de9b1e687536917fc6bcf3729
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-03-04 08:24:33 -0500

    close #2: add NoAutobackup from badi/mdq.git

--- mdprep/gmx.py
+++ mdprep/gmx.py
@@ -10,6 +10,7 @@ import subprocess
 import shlex
 
 __all__ = [
+    'NoAutobackup',
     'pdb2gmx',
     'grompp',
     'editconf',
@@ -17,6 +18,27 @@ __all__ = [
     'genbox',
     'mdrun',
 ]
+
+class NoAutobackup(object):
+    def __init__(self):
+        self._k = 'GMX_MAXBACKUP'
+        self._val = None
+
+    def disable_backups(self):
+        self.__enter__()
+
+    def enable_backups(self):
+        self.__exit__()
+
+    def __enter__(self):
+        if self._k in os.environ:
+            self._val   = os.environ[self._k]
+        os.environ[self._k] = '-1'
+
+    def __exit__(self, *args, **kws):
+        if self._val is not None:
+            os.environ[self._k] = self._val
+
 
 pdb2gmx  = process.optcmd('pdb2gmx')
 editconf = process.optcmd('editconf')
