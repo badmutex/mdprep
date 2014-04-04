ebc7a898dc74095b795042700909673fda646464
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-04-04 13:29:31 -0500

    update gmx module

--- mdprep/gmx.py
+++ mdprep/gmx.py
@@ -1,13 +1,7 @@
-from . import process
-from . import mdp
-from . import mdp_defaults
-from .util import StackDir
-from . import log
+from pxul.command import OptCommand
+from pxul.os import SetEnv
 
 import os
-import collections
-import subprocess
-import shlex
 
 __all__ = [
     'NoAutobackup',
@@ -19,31 +13,12 @@ __all__ = [
     'mdrun',
 ]
 
-class NoAutobackup(object):
-    def __init__(self):
-        self._k = 'GMX_MAXBACKUP'
-        self._val = None
+NoAutobackup = SetEnv(GMX_MAXBACKUP=-1)
 
-    def disable_backups(self):
-        self.__enter__()
-
-    def enable_backups(self):
-        self.__exit__()
-
-    def __enter__(self):
-        if self._k in os.environ:
-            self._val   = os.environ[self._k]
-        os.environ[self._k] = '-1'
-
-    def __exit__(self, *args, **kws):
-        if self._val is not None:
-            os.environ[self._k] = self._val
-
-
-pdb2gmx  = process.OptCommand('pdb2gmx')
-editconf = process.OptCommand('editconf')
-grompp   = process.OptCommand('grompp')
-genion   = process.OptCommand('genion')
-genbox   = process.OptCommand('genbox')
-mdrun    = process.OptCommand('mdrun')
+pdb2gmx  = OptCommand('pdb2gmx')
+editconf = OptCommand('editconf')
+grompp   = OptCommand('grompp')
+genion   = OptCommand('genion')
+genbox   = OptCommand('genbox')
+mdrun    = OptCommand('mdrun')
 
