715101361b822415061e865e559cced663284b53
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-04-04 16:00:17 -0400

    fix init imports

--- mdprep/__init__.py
+++ mdprep/__init__.py
@@ -1,11 +1,8 @@
-from .process import *
 from .gmx import *
 from .mdp import *
 from .experiment import *
 from .state import *
 import mdp_defaults
-import util
 import gmx_prep
 from . import _yaml as yaml
-from . import log
 from . import xpm
