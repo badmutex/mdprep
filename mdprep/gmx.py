52ec8ab230aee73fc8fe3d22b9c9fde608f2b7f1
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-07-25 13:05:39 -0400

    specify flag prefix

--- mdprep/gmx.py
+++ mdprep/gmx.py
@@ -15,10 +15,10 @@ __all__ = [
 
 NoAutobackup = SetEnv(GMX_MAXBACKUP=-1)
 
-pdb2gmx  = OptCommand('pdb2gmx')
-editconf = OptCommand('editconf')
-grompp   = OptCommand('grompp')
-genion   = OptCommand('genion')
-genbox   = OptCommand('genbox')
-mdrun    = OptCommand('mdrun')
+pdb2gmx  = OptCommand('pdb2gmx', short_flag_prefix='-', long_flag_prefix='-')
+editconf = OptCommand('editconf', short_flag_prefix='-', long_flag_prefix='-')
+grompp   = OptCommand('grompp', short_flag_prefix='-', long_flag_prefix='-')
+genion   = OptCommand('genion', short_flag_prefix='-', long_flag_prefix='-')
+genbox   = OptCommand('genbox', short_flag_prefix='-', long_flag_prefix='-')
+mdrun    = OptCommand('mdrun', short_flag_prefix='-', long_flag_prefix='-')
 
